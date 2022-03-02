1804c1b9caa7a9f32f1c3ba33f46bf4a6e732f70
rnakai <rnakai@student.42tokyo.jp>
2022-03-02 22:45:36 +0900

    serverがclientと疎通できるように

--- c_server/socket_server.cpp
+++ c_server/socket_server.cpp
@@ -4,6 +4,38 @@
 #include <iostream>
 #include <cstring>
 #include <unistd.h>
+#include "socket.hpp"
+
+static void TalkToClient(int iSocket) {
+    int  iRc, bNotDone;
+    char szBuf[256], szOk[] = "OK", szErr[] = "ERR";
+    bNotDone = true;
+
+    while (bNotDone) {
+        iRc = RecvLine(iSocket, szBuf, 256);
+        if (iRc < 0) {
+            std::cerr << "error recvline" << std::endl;
+            bNotDone = false;
+        }
+        if (strcmp(szBuf, "HELLO") == 0) {
+            iRc = send(iSocket, szOk, strlen(szOk), 0);
+            if (iRc < 0) {
+                std::cerr << "error send" << std::endl;
+                bNotDone = false;
+            }
+        } else if (strcmp(szBuf, "GOODBYE") == 0) {
+            if (iRc < 0) {
+                std::cerr << "error send" << std::endl;
+                bNotDone = false;
+            }
+            bNotDone = false;
+        } else {
+            send(iSocket, szErr, strlen(szErr), 0);
+            bNotDone = false;
+        }
+    }
+    close(iSocket);
+}
 
 int main(int argc, char* argv[]) {
     (void)argc;
@@ -23,21 +55,22 @@ int main(int argc, char* argv[]) {
     server.sin_addr.s_addr = INADDR_ANY;
     rc                     = bind(s, (struct sockaddr*)&server, len);
     if (rc < 0) {
-        std::cerr << "error" << std::endl;
+        std::cerr << "error bind" << std::endl;
         return 2;
     }
     rc = listen(s, SOMAXCONN);
     if (rc < 0) {
-        std::cerr << "error" << std::endl;
+        std::cerr << "error listen" << std::endl;
         return 3;
     }
     for (;;) {
         bzero(&client, len);
         c = accept(s, (struct sockaddr*)&client, (socklen_t*)&len);
         if (c < 0) {
-            std::cerr << "error" << std::endl;
+            std::cerr << "error accept" << std::endl;
             return 4;
         }
+        TalkToClient(c);
         close(c);
     }
 }
