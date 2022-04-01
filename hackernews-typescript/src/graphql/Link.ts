299eb3f2d8240011521ff615e532993450cd91f5
rnakai <rnakai@student.42tokyo.jp>
2022-04-01 10:45:42 +0900

    feedクエリの実装

--- hackernews-typescript/src/graphql/Link.ts
+++ hackernews-typescript/src/graphql/Link.ts
@@ -1,4 +1,5 @@
-import { objectType } from "nexus";
+import { extendType, objectType } from "nexus";
+import { NexusGenObjects } from "../../nexus-typegen";
 
 export const Link = objectType({
     name: "Link",
@@ -7,4 +8,29 @@ export const Link = objectType({
         t.nonNull.string("description");
         t.nonNull.string("url");
     }
+});
+
+let links: NexusGenObjects["Link"][] = [
+    {
+        id: 1,
+        url: "www.howtographql.com",
+        description: "Fullstack tutorial for GraphQL",
+    },
+    {
+        id: 2,
+        url: "graphql.org",
+        description: "GraphQL official website",
+    }
+];
+
+export const LinkQuery = extendType({
+    type: "Query",
+    definition(t) {
+        t.nonNull.list.nonNull.field("feed", {
+            type: "Link",
+            resolve(parent, args, context, info) {
+                return links;
+            },
+        });
+    },
 });
