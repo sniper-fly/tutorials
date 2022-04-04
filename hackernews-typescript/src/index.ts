e841714bc4830633777e17e8dedb4386526caa66
rnakai <rnakai@student.42tokyo.jp>
2022-04-04 10:32:48 +0900

    contextをリゾルバで使えるように

--- hackernews-typescript/src/index.ts
+++ hackernews-typescript/src/index.ts
@@ -1,9 +1,11 @@
 import { ApolloServer } from "apollo-server";
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core"
-
 import { schema } from "./schema";
+import { context } from "./context";
+
 export const server = new ApolloServer({
     schema,
+    context,
     plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
 });
 
