03d9997bf8f02f4a77537ea7e1f820892dc751c0
rnakai <rnakai@student.42tokyo.jp>
2022-04-04 10:53:24 +0900

    crudのうちcrをｄｂに接続

--- hackernews-typescript/nexus-typegen.ts
+++ hackernews-typescript/nexus-typegen.ts
@@ -4,7 +4,7 @@
  */
 
 
-
+import type { Context } from "./src/context"
 
 
 
@@ -132,7 +132,7 @@ export type NexusGenFeaturesConfig = {
 }
 
 export interface NexusGenTypes {
-  context: any;
+  context: Context;
   inputTypes: NexusGenInputs;
   rootTypes: NexusGenRootTypes;
   inputTypeShapes: NexusGenInputs & NexusGenEnums & NexusGenScalars;
