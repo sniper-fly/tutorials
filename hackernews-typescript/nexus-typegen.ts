bce14628236ae1d3ce545dc84a7c6f8f391e731a
rnakai <rnakai@student.42tokyo.jp>
2022-04-08 18:54:53 +0900

    authを追加

--- hackernews-typescript/nexus-typegen.ts
+++ hackernews-typescript/nexus-typegen.ts
@@ -28,6 +28,10 @@ export interface NexusGenScalars {
 }
 
 export interface NexusGenObjects {
+  AuthPayload: { // root type
+    token: string; // String!
+    user: NexusGenRootTypes['User']; // User!
+  }
   Link: { // root type
     description: string; // String!
     id: number; // Int!
@@ -53,6 +57,10 @@ export type NexusGenRootTypes = NexusGenObjects
 export type NexusGenAllTypes = NexusGenRootTypes & NexusGenScalars
 
 export interface NexusGenFieldTypes {
+  AuthPayload: { // field return type
+    token: string; // String!
+    user: NexusGenRootTypes['User']; // User!
+  }
   Link: { // field return type
     description: string; // String!
     id: number; // Int!
@@ -62,7 +70,9 @@ export interface NexusGenFieldTypes {
   Mutation: { // field return type
     DeleteLink: NexusGenRootTypes['Link']; // Link!
     GetLink: NexusGenRootTypes['Link']; // Link!
+    login: NexusGenRootTypes['AuthPayload']; // AuthPayload!
     post: NexusGenRootTypes['Link']; // Link!
+    signup: NexusGenRootTypes['AuthPayload']; // AuthPayload!
     updateLink: NexusGenRootTypes['Link']; // Link!
   }
   Query: { // field return type
@@ -77,6 +87,10 @@ export interface NexusGenFieldTypes {
 }
 
 export interface NexusGenFieldTypeNames {
+  AuthPayload: { // field return type name
+    token: 'String'
+    user: 'User'
+  }
   Link: { // field return type name
     description: 'String'
     id: 'Int'
@@ -86,7 +100,9 @@ export interface NexusGenFieldTypeNames {
   Mutation: { // field return type name
     DeleteLink: 'Link'
     GetLink: 'Link'
+    login: 'AuthPayload'
     post: 'Link'
+    signup: 'AuthPayload'
     updateLink: 'Link'
   }
   Query: { // field return type name
@@ -108,9 +124,18 @@ export interface NexusGenArgTypes {
     GetLink: { // args
       id: number; // Int!
     }
+    login: { // args
+      email: string; // String!
+      password: string; // String!
+    }
     post: { // args
       description: string; // String!
       url: string; // String!
+    }
+    signup: { // args
+      email: string; // String!
+      name: string; // String!
+      password: string; // String!
     }
     updateLink: { // args
       description?: string | null; // String
