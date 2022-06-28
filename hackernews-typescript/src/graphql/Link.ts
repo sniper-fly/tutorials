dbaa11574c9a174d5aa502e35937103905b21257
rnakai <rnakai@student.42tokyo.jp>
2022-06-28 22:11:56 +0900

    update Feed

--- hackernews-typescript/src/graphql/Link.ts
+++ hackernews-typescript/src/graphql/Link.ts
@@ -28,18 +28,27 @@ export const Link = objectType({
   }
 });
 
+export const Feed = objectType({
+  name: "Feed",
+  definition(t) {
+    t.nonNull.list.nonNull.field("links", { type: Link });
+    t.nonNull.int("count");
+    t.id("id");
+  }
+});
+
 export const LinkQuery = extendType({
   type: "Query",
   definition(t) {
-    t.nonNull.list.nonNull.field("feed", {
-      type: "Link",
+    t.nonNull.field("feed", {
+      type: "Feed",
       args: {
         filter: stringArg(),
         skip: intArg(),
         take: intArg(),
         orderBy: arg({ type: list(nonNull(LinkOrderByInput)) })
       },
-      resolve(parent, args, context, info) {
+      async resolve(parent, args, context) {
         const where = args.filter
           ? {
               OR: [
@@ -48,12 +57,22 @@ export const LinkQuery = extendType({
               ],
           }
           : {};
-        return context.prisma.link.findMany({
+        const links = context.prisma.link.findMany({
           where,
           skip: args?.skip as number | undefined,
           take: args?.take as number | undefined,
-          orderBy: args?.orderBy as Prisma.Enumerable<Prisma.LinkOrderByWithRelationInput> | undefined
+          orderBy: args?.orderBy as
+            | Prisma.Enumerable<Prisma.LinkOrderByWithRelationInput>
+            | undefined
         });
+        const count = await context.prisma.link.count({ where });
+        const id = `main-feed:${JSON.stringify(args)}`;
+
+        return {
+          links,
+          count,
+          id
+        }
       }
     });
   }
