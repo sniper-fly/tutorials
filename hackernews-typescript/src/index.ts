b0eb87e8569892c75690424fc411d0c7d36719a8
rnakai <rnakai@student.42tokyo.jp>
2022-06-28 22:51:16 +0900

    package.jsonやdocker-composeを整備

--- hackernews-typescript/src/index.ts
+++ hackernews-typescript/src/index.ts
@@ -1,13 +1,16 @@
 import { ApolloServer } from "apollo-server";
+import { ApolloServerPluginLandingPageLocalDefault } from "apollo-server-core";
 import { context } from "./context";
 import { schema } from "./schema";
 
 export const server = new ApolloServer({
   schema,
   context,
+  introspection: true,
+  plugins: [ApolloServerPluginLandingPageLocalDefault()]
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 server.listen({port}).then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
