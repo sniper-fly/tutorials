085f48e8d5d4e482845043b559cc44ef0915d4a7
rnakai <rnakai@student.42tokyo.jp>
2022-11-15 10:50:29 +0900

    コンポーネントをファイルに分割

--- StickerSmash/App.js
+++ StickerSmash/App.js
@@ -1,5 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+
+import ImageViewer from './components/ImageViewer';
 
 const PlaceholderImage = require('./assets/images/background-image.png')
 
@@ -7,7 +9,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <Image source={PlaceholderImage} style={styles.image} />
+        <ImageViewer placeholderImageSource={PlaceholderImage} />
       </View>
       <StatusBar style="auto" />
     </View>
