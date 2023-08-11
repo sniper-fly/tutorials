81113a64b03c280f8827c4ff01f23efabec8a1ad
rnakai <rnakai@student.42tokyo.jp>
2023-08-11 15:21:27 +0900

    reactでxを押すマス目の完成

--- tictactoe_rta/my-app/src/index.js
+++ tictactoe_rta/my-app/src/index.js
@@ -1,19 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = "X";
+    this.setState({ squares: squares });
+  }
+
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
+  }
+
   render() {
     return (
       <div>
-        <div className='board-row'>
-
+        <div className="board-row">
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+          {this.renderSquare(3)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+          {this.renderSquare(6)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+          {this.renderSquare(9)}
         </div>
       </div>
-    )
+    );
   }
 }
 
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Board/>)
+root.render(<Board />);
