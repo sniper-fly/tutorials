717f9a831f1ed59c4dd63bd85d12c469e323db5e
rnakai <rnakai@student.42tokyo.jp>
2023-09-05 00:28:11 +0900

    ex2

---
+++ react_9_simple_exercises/mine/src/components/Button.tsx
@@ -0,0 +1,5 @@
+export const Button: React.FC = () => {
+  return <button onClick={() => alert('clicked!')}>
+    Click Me
+  </button>
+}
--- react_9_simple_exercises/mine/src/index.tsx
+++ react_9_simple_exercises/mine/src/index.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Center from './components/Center';
+import { Button } from './components/Button';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Center/>
+    <Button/>
   </React.StrictMode>
 );
 
