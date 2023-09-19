106b824f5a860ebb60251f398cf2d8104bfefc51
rnakai <rnakai@student.42tokyo.jp>
2023-09-19 23:58:31 +0900

    全てのリクエストにリクエストヘッダ認証ができるように

--- hackernews-react-apollo/src/index.js
+++ hackernews-react-apollo/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
 import { BrowserRouter } from 'react-router-dom';
+import { setContext } from '@apollo/client/link/context';
+import { AUTH_TOKEN } from './constants';
 
 import {
   ApolloProvider,
@@ -15,8 +17,18 @@ const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN);
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 });
 
