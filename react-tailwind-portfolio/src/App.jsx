f1f9c19ea0080357898c17c8f465ed3226bb4622
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2023-10-14 21:13:58 +0900

    main section style適用終わり

--- react-tailwind-portfolio/src/App.jsx
+++ react-tailwind-portfolio/src/App.jsx
@@ -25,22 +25,25 @@ function App() {
           </nav>
         </div>
       </header>
-      <section>
-        <div>
-          <div>
-            <h1>
+
+      <section className="text-gray-700" id="home">
+        <div className="flex container mx-auto py-20 px-5 flex-col md:flex-row items-center">
+          <div className="md:w-1/2 flex-grow mb-16 lg:pr-24 md:pr-16 text-center md:text-left">
+            <h1 className="text-3xl sm:text-6xl text-gray-900 font-medium mb-4">
               Hi!
               <br />
               I'm RyoCode
               <br />
               Web Developer
             </h1>
-            <p>
+            <p className="mb-8 leading-relaxed">
               lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
             </p>
-            <button>Contact me</button>
+            <button className="text-white bg-green-500 py-2 px-6 border-0 rounded text-lg hover:bg-green-600 duration-300">
+              Contact
+            </button>
           </div>
-          <div>
+          <div className="md:w-1/2 lg:max-w-lg w-5/6">
             <img src="./img/icon.jpg" alt="icon" />
           </div>
         </div>
