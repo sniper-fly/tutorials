a0b793cddd7da0ebd4c7378b4ee9bec5abc79d18
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2023-10-15 23:20:13 +0900

    skillの表示

--- react-tailwind-portfolio/src/App.jsx
+++ react-tailwind-portfolio/src/App.jsx
@@ -268,6 +268,65 @@ function App() {
           </div>
         </div>
       </section>
+
+      <section className="text-gray-700 border-t border-gray-200">
+        <div className="container px-5 py-24 mx-auto flex flex-wrap">
+          {/* left side */}
+          <div className="mb-10 lg:mb-0 w-full lg:w-1/2 flex justify-center">
+            <img src="./img/pc.jpg" alt="" className="rounded" />
+          </div>
+          {/* right side */}
+          <div className="lg:pl-12 lg:py-6 w-full lg:w-1/2">
+            <h1 className="text-2xl sm:text-3xl font-medium text-gray-900 mb-10">
+              My Skills
+            </h1>
+            <div>
+              <h2>HTML</h2>
+              <div className="shadow bg-green-100 mt-2 w-full">
+                <div
+                  className="text-xs leading-none py-1 text-center text-white bg-green-900"
+                  style={{ width: "85%" }}
+                >
+                  85%
+                </div>
+              </div>
+            </div>
+            <div>
+              <h2>CSS</h2>
+              <div className="shadow bg-green-100 mt-2 w-full">
+                <div
+                  className="text-xs leading-none py-1 text-center text-white bg-green-900"
+                  style={{ width: "65%" }}
+                >
+                  65%
+                </div>
+              </div>
+            </div>
+            <div>
+              <h2>Javascript</h2>
+              <div className="shadow bg-green-100 mt-2 w-full">
+                <div
+                  className="text-xs leading-none py-1 text-center text-white bg-green-900"
+                  style={{ width: "70%" }}
+                >
+                  70%
+                </div>
+              </div>
+            </div>
+            <div>
+              <h2>React</h2>
+              <div className="shadow bg-green-100 mt-2 w-full">
+                <div
+                  className="text-xs leading-none py-1 text-center text-white bg-green-900"
+                  style={{ width: "50%" }}
+                >
+                  50%
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
