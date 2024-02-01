480ef2fd8919a4b54f19d7bb0d6df77d7a098933
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-02-01 10:19:13 +0900

    format

--- spotify-profile-demo/src/script.ts
+++ spotify-profile-demo/src/script.ts
@@ -9,7 +9,7 @@ if (!code) {
 } else {
   const accessToken = await getAccessToken(clientId, code);
   const profile = await fetchProfile(accessToken);
-  console.log(profile)
+  console.log(profile);
   populateUI(profile);
 }
 
@@ -24,17 +24,20 @@ async function fetchProfile(token: string): Promise<any> {
 
 function populateUI(profile: any) {
   // TODO: Update UI with profile data
-    document.getElementById("displayName")!.innerText = profile.display_name;
-    if (profile.images[0]) {
-        const profileImage = new Image(200, 200);
-        profileImage.src = profile.images[0].url;
-        document.getElementById("avatar")!.appendChild(profileImage);
-    }
-    document.getElementById("id")!.innerText = profile.id;
-    document.getElementById("email")!.innerText = profile.email;
-    document.getElementById("uri")!.innerText = profile.uri;
-    document.getElementById("uri")!.setAttribute("href", profile.external_urls.spotify);
-    document.getElementById("url")!.innerText = profile.href;
-    document.getElementById("url")!.setAttribute("href", profile.href);
-    document.getElementById("imgUrl")!.innerText = profile.images[0]?.url ?? '(no profile image)';
+  document.getElementById("displayName")!.innerText = profile.display_name;
+  if (profile.images[0]) {
+    const profileImage = new Image(200, 200);
+    profileImage.src = profile.images[0].url;
+    document.getElementById("avatar")!.appendChild(profileImage);
+  }
+  document.getElementById("id")!.innerText = profile.id;
+  document.getElementById("email")!.innerText = profile.email;
+  document.getElementById("uri")!.innerText = profile.uri;
+  document
+    .getElementById("uri")!
+    .setAttribute("href", profile.external_urls.spotify);
+  document.getElementById("url")!.innerText = profile.href;
+  document.getElementById("url")!.setAttribute("href", profile.href);
+  document.getElementById("imgUrl")!.innerText =
+    profile.images[0]?.url ?? "(no profile image)";
 }
