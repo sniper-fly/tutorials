a99044e7486c5176cb284968d8d07c26e3dd5ab2
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-02-13 17:15:44 +0900

    not-foundとerror.tsxを追加

--- nextjs-dashboard/app/dashboard/invoices/[id]/edit/page.tsx
+++ nextjs-dashboard/app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,7 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
 
 type Props = {
   params: { id: string };
@@ -12,6 +13,10 @@ export default async function Page({ params }: Props) {
     fetchInvoiceById(id),
     fetchCustomers(),
   ]);
+
+  if (!invoice) {
+    notFound();
+  }
 
   return (
     <main>
