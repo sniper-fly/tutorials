20086c0412cb16deb666163b65d2851eb29333f5
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-02-24 18:33:14 +0900

    メタデータの追加

--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -1,5 +1,22 @@
-import '@/app/ui/global.css'
+import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+  // alternates: {
+  //   canonical: '/',
+  //   languages: {
+  //     'en-US': '/en-US',
+  //     'de-DE': '/de-DE',
+  //   },
+  // },
+};
 
 export default function RootLayout({
   children,
