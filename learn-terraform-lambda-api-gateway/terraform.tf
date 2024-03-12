842f50befd35cd394841d59bbf082d7f2e55d506
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-03-12 17:40:34 +0900

    lambda用のソースコードzipをbucketにデプロイ

---
+++ learn-terraform-lambda-api-gateway/terraform.tf
@@ -0,0 +1,29 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+terraform {
+
+  cloud {
+    organization = "terraform_tutorial_miroscular"
+    workspaces {
+      name = "learn-terraform-lambda-api-gateway"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.38.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.4.2"
+    }
+  }
+
+  required_version = "~> 1.2"
+}
