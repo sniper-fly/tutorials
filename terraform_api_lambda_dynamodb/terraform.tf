3ecf5b9b58b82cd8e739503e3af8166139881adf
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-03-15 14:19:21 +0900

    init project

---
+++ terraform_api_lambda_dynamodb/terraform.tf
@@ -0,0 +1,25 @@
+terraform {
+  cloud {
+    organization = "terraform_tutorial_miroscular"
+    workspaces {
+      name = "learning_with_cli"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.41.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.4.2"
+    }
+  }
+
+  required_version = "~> 1.7"
+}
