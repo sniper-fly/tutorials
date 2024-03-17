b4dca60841426b424233d44fa72f8177a0157e46
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-03-17 18:15:28 +0900

    middlewareで直接リクエストを生成

--- nextjs-dashboard/middleware.ts
+++ nextjs-dashboard/middleware.ts
@@ -12,19 +12,11 @@ export const config = {
 };
 
 export function middleware(request: NextRequest) {
-  // Clone the request headers and set a new header `x-hello-from-middleware1`
-  const requestHeaders = new Headers(request.headers);
-  requestHeaders.set('x-hello-from-middleware1', 'hello');
-
-  // You can also set request headers in NextResponse.rewrite
-  const response = NextResponse.next({
-    request: {
-      // New request headers
-      headers: requestHeaders,
-    },
-  });
-
-  // Set a new response header `x-hello-from-middleware2`
-  response.headers.set('x-hello-from-middleware2', 'hello');
-  return response;
+  // 先頭が /hoge の場合
+  if (request.nextUrl.pathname.startsWith('/hoge')) {
+    return Response.json(
+      { success: false, message: 'authentication failed' },
+      { status: 401 },
+    );
+  }
 }
