2cf7e373a5bba6b636d00b1e9513268f4ec52a93
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-03-19 17:28:18 +0900

    translate apiを利用できるようにコード変更

--- terraform_api_lambda_dynamodb/lambda/hello.py
+++ terraform_api_lambda_dynamodb/lambda/hello.py
@@ -1,9 +1,16 @@
 import json
-import logging
+import boto3
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+translate = boto3.client("translate")
+
 
 def lambda_handler(event, context):
-    logger.info(event)
-    return {"statusCode": 200, "body": json.dumps("Hello Hands on world!")}
+    input_text = "おはよう"
+
+    response = translate.translate_text(
+        Text=input_text, SourceLanguageCode="ja", TargetLanguageCode="en"
+    )
+
+    output_text = response.get("TranslatedText")
+
+    return {"statusCode": 200, "body": json.dumps({"output_text": output_text})}
