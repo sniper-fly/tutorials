caa80ac659c4e52f98e2981647b062a2c8ad30c1
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-03-25 17:10:02 +0900

    apiのデプロイとroute, ステージの作成

---
+++ terraform_api_lambda_dynamodb/api_gateway.tf
@@ -0,0 +1,24 @@
+resource "aws_apigatewayv2_api" "lambda" {
+  name          = "serverless_lambda_gw"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "example" {
+  api_id           = aws_apigatewayv2_api.lambda.id
+  integration_type = "AWS_PROXY"
+  integration_uri    = aws_lambda_function.hello_world.invoke_arn
+}
+
+resource "aws_apigatewayv2_route" "hello_world" {
+  api_id = aws_apigatewayv2_api.lambda.id
+
+  route_key = "GET /hello"
+  target    = "integrations/${aws_apigatewayv2_integration.example.id}"
+}
+
+resource "aws_apigatewayv2_stage" "lambda" {
+  api_id = aws_apigatewayv2_api.lambda.id
+
+  name        = "serverless_lambda_stage"
+  auto_deploy = true
+}
