f8de340b759b8ba771425cda3751a6ed775bf08e
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-03-28 16:20:52 +0900

    create mock integration

---
+++ terraform_api_lambda_dynamodb/mock_rest_api.tf
@@ -0,0 +1,50 @@
+resource "aws_api_gateway_rest_api" "example" {
+  name = "example"
+}
+
+resource "aws_api_gateway_resource" "example" {
+  rest_api_id = aws_api_gateway_rest_api.example.id
+  parent_id   = aws_api_gateway_rest_api.example.root_resource_id
+  path_part   = "hello"
+}
+
+resource "aws_api_gateway_method" "example" {
+  rest_api_id   = aws_api_gateway_rest_api.example.id
+  resource_id   = aws_api_gateway_resource.example.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "example2" {
+  rest_api_id = aws_api_gateway_rest_api.example.id
+  resource_id = aws_api_gateway_resource.example.id
+  http_method = aws_api_gateway_method.example.http_method
+  type        = "MOCK"
+
+  request_templates = {
+        "application/json" = "{ \"statusCode\": 200 }"
+  }
+}
+
+resource "aws_api_gateway_method_response" "example" {
+  rest_api_id = aws_api_gateway_rest_api.example.id
+  resource_id = aws_api_gateway_resource.example.id
+  http_method = aws_api_gateway_method.example.http_method
+  status_code = 200
+}
+
+resource "aws_api_gateway_integration_response" "example" {
+  rest_api_id = aws_api_gateway_rest_api.example.id
+  resource_id = aws_api_gateway_resource.example.id
+  http_method = aws_api_gateway_method.example.http_method
+  status_code = aws_api_gateway_method_response.example.status_code
+
+  response_templates = {
+    "application/json": <<EOF
+{
+  "report_id": 5,
+  "report_title" : "Hello, world"
+}
+EOF
+  }
+}
