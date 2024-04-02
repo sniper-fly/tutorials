e398943677c89d35aa34e2ef549ac0c68f381ff0
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-04-02 15:06:34 +0900

    s3のpublic access blockを解除

--- aws_static_web_s3/s3.tf
+++ aws_static_web_s3/s3.tf
@@ -1,3 +1,12 @@
-resource "aws_s3_bucket" "s3_bucket_name" {
+resource "aws_s3_bucket" "anitunes_click" {
   bucket = "anitunes.click"
+}
+
+resource "aws_s3_bucket_public_access_block" "anitunes_click" {
+  bucket = aws_s3_bucket.anitunes_click.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
