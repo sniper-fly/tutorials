7e0ce73280155e3539f459f72637b5dbaeb3f58f
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-04-02 14:58:49 +0900

    bucket名変更

--- aws_static_web_s3/s3.tf
+++ aws_static_web_s3/s3.tf
@@ -1,9 +1,3 @@
-resource "random_pet" "s3_bucket_name" {
-  prefix = "aws-handson-static-website"
-  length = 2
-  separator = "-"
-}
-
 resource "aws_s3_bucket" "s3_bucket_name" {
-  bucket = random_pet.s3_bucket_name.id
+  bucket = "anitunes.click"
 }
