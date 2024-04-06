f922354fcebf07a24d75ad1733bd7ee41bf4be1a
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-04-06 14:58:05 +0900

    cloud frontの設定

---
+++ aws_static_web_s3/cloudfront.tf
@@ -0,0 +1,38 @@
+locals {
+  s3_origin_id = "anitunes.click"
+}
+
+data "aws_cloudfront_cache_policy" "caching_optimized" {
+  name = "Managed-CachingOptimized"
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.anitunes_click.bucket_regional_domain_name
+    origin_id   = local.s3_origin_id
+  }
+
+  enabled             = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    cache_policy_id = data.aws_cloudfront_cache_policy.caching_optimized.id
+
+    viewer_protocol_policy = "allow-all"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations        = ["US", "CA", "GB", "DE", "FR", "IN", "JP"]
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
