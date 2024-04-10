8e5960f71f108483dd8be4bd55c1ade7acb6fd5c
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-04-10 18:16:48 +0900

    DNSvalidationを行って証明書発行まで自動化

--- aws_static_web_s3/main.tf
+++ aws_static_web_s3/main.tf
@@ -33,3 +33,8 @@ provider "aws" {
     }
   }
 }
+
+provider "aws" {
+  region = "us-east-1"
+  alias = "virginia"
+}
--- aws_static_web_s3/route53.tf
+++ aws_static_web_s3/route53.tf
@@ -16,6 +16,38 @@ resource "aws_route53_record" "anitunes_click" {
   }
 }
 
-output "anitunes_click" {
-  value = aws_s3_bucket_website_configuration.anitunes_click.website_domain
+resource "aws_acm_certificate" "cert" {
+  domain_name       = "anitunes.click"
+  validation_method = "DNS"
+
+  provider = aws.virginia
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options :
+    dvo.domain_name => {
+      name    = dvo.resource_record_name
+      type    = dvo.resource_record_type
+      records   = dvo.resource_record_value
+    }
+  }
+
+  allow_overwrite = true
+  zone_id = aws_route53_record.anitunes_click.zone_id
+  name    = each.value.name
+  type    = each.value.type
+  records = [each.value.records]
+  ttl     = 60
+}
+
+resource "aws_acm_certificate_validation" "cert" {
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+
+  provider = aws.virginia
 }
