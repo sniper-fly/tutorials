be9e739740d2c18b7269c8fa444e7a740edb5ae7
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-04-11 14:16:17 +0900

    ドメインからcloudfrontにリダイレクトするように

--- aws_static_web_s3/cloudfront.tf
+++ aws_static_web_s3/cloudfront.tf
@@ -11,6 +11,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     domain_name = aws_s3_bucket.anitunes_click.bucket_regional_domain_name
     origin_id   = local.s3_origin_id
   }
+  aliases = ["anitunes.click"]
 
   enabled             = true
   default_root_object = "index.html"
@@ -22,7 +23,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
 
     cache_policy_id = data.aws_cloudfront_cache_policy.caching_optimized.id
 
-    viewer_protocol_policy = "allow-all"
+    viewer_protocol_policy = "redirect-to-https"
   }
 
   restrictions {
@@ -33,6 +34,8 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   }
 
   viewer_certificate {
-    cloudfront_default_certificate = true
+    acm_certificate_arn = aws_acm_certificate.cert.arn
+    ssl_support_method = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2021"
   }
 }
--- aws_static_web_s3/route53.tf
+++ aws_static_web_s3/route53.tf
@@ -8,10 +8,10 @@ resource "aws_route53_record" "anitunes_click" {
   type    = "A"
 
   alias {
-    # name                   = aws_cloudfront_distribution.s3_distribution.domain_name
-    # zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
-    name = aws_s3_bucket_website_configuration.anitunes_click.website_domain
-    zone_id = aws_s3_bucket.anitunes_click.hosted_zone_id
+    name                   = aws_cloudfront_distribution.s3_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
+    # name = aws_s3_bucket_website_configuration.anitunes_click.website_domain
+    # zone_id = aws_s3_bucket.anitunes_click.hosted_zone_id
     evaluate_target_health = false
   }
 }
