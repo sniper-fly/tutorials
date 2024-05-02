20e07f333b4413f8b3e84c733c35ec8a7c105e44
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-05-02 21:42:58 +0900

    aws 公式にtsのコードを合わせる

--- terraform_lambdazip_provider/lambda/src/index.ts
+++ terraform_lambdazip_provider/lambda/src/index.ts
@@ -1,8 +1,20 @@
 import axios from "axios";
+// const url = "https://api.animethemes.moe/anime";
+// const res = await axios.get(url);
+// console.log(res.data);
 
-export const handler = (event, context) => {
-  // const url = "https://api.animethemes.moe/anime";
-  // const res = await axios.get(url);
-  // console.log(res.data);
-  return context.logStreamName;
+import { Context, APIGatewayProxyResult, APIGatewayEvent } from "aws-lambda";
+
+export const handler = async (
+  event: APIGatewayEvent,
+  context: Context
+): Promise<APIGatewayProxyResult> => {
+  console.log(`Event: ${JSON.stringify(event, null, 2)}`);
+  console.log(`Context: ${JSON.stringify(context, null, 2)}`);
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "hello world",
+    }),
+  };
 };
