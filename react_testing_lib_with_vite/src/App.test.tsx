9c2552e4f2fa8cb909b1d1b05c7ed7ae24cc6978
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-07-31 18:26:52 +0900

    check if App render App component

--- react_testing_lib_with_vite/src/App.test.tsx
+++ react_testing_lib_with_vite/src/App.test.tsx
@@ -1,11 +1,10 @@
 import { describe, it, expect } from 'vitest';
+import { render } from '@testing-library/react';
 
-describe('something truthy and falsy', () => {
-  it('true to be true', () => {
-    expect(true).toBe(true);
-  });
+import App from './App';
 
-  it('false to be false', () => {
-    expect(false).toBe(false);
+describe('renders App component', () => {
+  it('renders App component', () => {
+    render(<App />);
   });
 });
