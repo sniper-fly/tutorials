0ae07b7e6aa0af472a227450cd2c36d3f5021988
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-07-31 18:37:58 +0900

    learn screen.debug

--- react_testing_lib_with_vite/src/App.test.tsx
+++ react_testing_lib_with_vite/src/App.test.tsx
@@ -1,10 +1,12 @@
 import { describe, it, expect } from 'vitest';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import App from './App';
 
 describe('renders App component', () => {
   it('renders App component', () => {
     render(<App />);
+
+    screen.debug();
   });
 });
--- react_testing_lib_with_vite/src/App.tsx
+++ react_testing_lib_with_vite/src/App.tsx
@@ -1,9 +1,38 @@
-import "./App.css";
-
-const title = "Hello React";
+import * as React from "react";
 
 function App() {
-  return <div>{title}</div>;
+  const [search, setSearch] = React.useState("");
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setSearch(event.target.value);
+  }
+
+  return (
+    <div>
+      <Search value={search} onChange={handleChange}>
+        Search:
+      </Search>
+
+      <p>Searches for {search ? search : "..."}</p>
+    </div>
+  );
+}
+
+function Search({
+  value,
+  onChange,
+  children,
+}: {
+  value: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  children: React.ReactNode;
+}) {
+  return (
+    <div>
+      <label htmlFor="search">{children}</label>
+      <input id="search" type="text" value={value} onChange={onChange} />
+    </div>
+  );
 }
 
 export default App;
